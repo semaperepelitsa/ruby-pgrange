07fc7d355a2d20990a2b2b2a04ec98bbb2453755
Semyon Perepelitsa <sema@sema.in>
2014-06-22 14:06:18 +0800

    Move test.

--- lib/pgrange.rb
+++ lib/pgrange.rb
@@ -1,6 +1,3 @@
-gem 'minitest', '~> 5.0'
-require "minitest/autorun"
-
 class PGRange
   BOUNDS = {
     '[]' => [true, true],
@@ -61,39 +58,4 @@ class PGRange
   # Range compatibility
   alias_method :begin, :lower
   alias_method :end, :upper
-end
-
-class RangeTest < Minitest::Test
-  def test_include
-    rng = PGRange.new(1, 3, '[)')
-    assert rng.include?(1)
-    assert rng.include?(2)
-    refute rng.include?(3)
-    refute rng.include?(0)
-  end
-
-  def test_wrong_bounds
-    assert_raises ArgumentError do
-      PGRange.new(1, 3, 'foo')
-    end
-  end
-
-  def test_to_s
-    rng = PGRange.new(1, 3, '[)')
-    assert_equal "[1,3)", rng.to_s
-    assert_equal "[1,3)", rng.inspect
-  end
-
-  def test_empty
-    rng = PGRange.new(1, 1, '[)')
-    assert rng.empty?
-    assert_equal nil, rng.upper
-    assert_equal nil, rng.lower
-    refute rng.lower_inc?
-    refute rng.upper_inc?
-    assert_equal "empty", rng.to_s
-
-    rng = PGRange.new(1, 1, '[]')
-    refute rng.empty?
-  end
 end
